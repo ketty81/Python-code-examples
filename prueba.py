1832ff59ff07e82bd046ecee5f22e2b947d4fbfd
ketty <ketty810614@gmail.com>
2017-10-04 21:04:51 -0500

    Ultima Version

--- prueba.py
+++ prueba.py
@@ -5,18 +5,13 @@ from configurations.models import *
 fecha = raw_input('Fecha formato dd-mm-aaaa:')
 placa = raw_input('Placa de Vehiculo:')
 horario = raw_input('Horario formato hh:mm:ss:')
-vehiculo = None
-
 
 configuracion = ConfiguracionPicoPlaca()
-
-
 try:
     vehiculo = Vehiculo(placa)
 except Exception as error:
     print(error)
     exit()
-
 
 try:
     if vehiculo.puede_circular(fecha,horario,configuracion):
